a314773a339a2f23c322ef80b51e8a316ec20c3e
Oleh Polishchuk <oleh.polishchuk.ck@gmail.com>
2018-01-07 14:36:38 +0200

    Add reactive form validation on "create new product" modal window

--- src/app/product-modal/product-modal.component.ts
+++ src/app/product-modal/product-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material";
+import { FormControl, FormGroup, NgForm } from "@angular/forms";
 import { Product } from "../product/product";
 
 @Component({
@@ -9,16 +10,27 @@ import { Product } from "../product/product";
 })
 export class ProductModalComponent implements OnInit {
 
+  private productForm: FormGroup;
   product: Product = new Product();
 
   constructor(public dialogRef: MatDialogRef<ProductModalComponent>,
               @Inject(MAT_DIALOG_DATA) public data: any) { }
 
   ngOnInit() {
+    this.productForm = new FormGroup({
+      name: new FormControl(),
+      price: new FormControl()
+    });
   }
 
-  onNoClick(): void {
+  cancel(): void {
     this.dialogRef.close();
+  }
+
+  save(productForm: NgForm): void {
+    if (productForm.dirty && productForm.valid) {
+      this.dialogRef.close(productForm.value);
+    }
   }
 
 }
