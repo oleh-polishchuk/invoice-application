d2f87e3ba4363f3133dc3820c01a8ccef836ffae
Oleh Polishchuk <oleh.polishchuk.ck@gmail.com>
2018-01-15 22:43:52 +0200

    Removed reactive-form validation

--- src/app/invoice/invoice.component.ts
+++ src/app/invoice/invoice.component.ts
@@ -211,8 +211,10 @@ export class InvoiceComponent implements OnInit {
   saveInvoice(): void {
     if (this.invoice && this.invoice.isValidToSave()) {
       this.invoiceService.saveInvoice(this.invoice)
-        .subscribe(invoice => this.saveInvoiceItems(invoice));
-      this.router.navigate([ '/invoices' ]);
+        .subscribe(invoice => {
+          this.saveInvoiceItems(invoice);
+          this.router.navigate([ '/invoices' ]);
+        });
     }
   }
 
